d2c706535f99f01f736be19126bcae22c9c862c3
suganya1317 <sugi.suganya80@gmail.com>
2018-05-15 23:28:22 +0530

    verifyemail

--- application/views/verify_email.php
+++ application/views/verify_email.php
@@ -1,5 +1,5 @@
 <div class="sign-in-wrap col-md-6 col-md-offset-1 col-sm-6 col-sm-offset-1">
     <div id="sign-in-form" class="sign-form" tabindex="-1" role="dialog">
-        <div>Successfully verified. Please <a href="<?php echo site_url() ?>">login</a></div>
+        <div>Thank You For registering in jithire, Please <a href="<?php echo site_url() ?>">login to you email and activate the account</a></div>
     </div>
 </div>
